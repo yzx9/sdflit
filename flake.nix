425f83e04381ab43220158bc96dc7919c7bcbae2
Zexin Yuan <git@yzx9.xyz>
2025-01-03 23:57:17 +0800

    chore: add nix flake and direnv

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "sdflit";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      fenix,
+      ...
+    }:
+
+    let
+      inherit (nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem = lib.genAttrs supportedSystems;
+    in
+    {
+      # nix develop
+      devShells = forEachSupportedSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              fenix.overlays.default
+            ];
+          };
+
+          fenix' = fenix.packages.${system};
+          toolchain = fenix'.complete;
+          rust = fenix'.combine [
+            toolchain.cargo
+            toolchain.rustc
+            toolchain.rust-src
+            toolchain.clippy
+            toolchain.rustfmt
+          ];
+
+          # python
+          python = pkgs.python312;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # rust
+              rust
+              rust-analyzer
+              python
+              just
+            ];
+
+            shellHook = ''
+              # Specify the rust-src path (many editors rely on this)
+              export RUST_SRC_PATH="${toolchain.rust-src}/lib/rustlib/src/rust/library";
+            '';
+          };
+        }
+      );
+    };
+}
